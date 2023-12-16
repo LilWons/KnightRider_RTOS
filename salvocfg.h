3e19a1feae7a779737fc80b5929a32b24c088698
LilWons <richardlhart@outlook.com>
2023-12-15 22:33:30 -0500

    Removed file RtosMain.c salvocfg.h

--- salvocfg.h
+++
@@ -1,9 +0,0 @@
-
-#define OSUSE_LIBRARY			TRUE
-#define OSLIBRARY_TYPE			OSL
-#define OSLIBRARY_CONFIG		OST
-#define OSEVENTS                5
-#define OSEVENT_FLAGS           0
-#define OSMESSAGE_QUEUES        2
-#define OSTASKS					9
-
